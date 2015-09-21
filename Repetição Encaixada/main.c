ae6a96ba9c54e050a136072ffff45c4a0df92aa4
daviwesley <davi.wesley@icloud.com>
2015-09-21 13:54:39 -0300

    Priimeiro passo

---
+++ Repetição Encaixada/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Dados dois naturais n>0 e m>0, determinar entre todos os pares de números inteiros (x,y)
+ tias que 0 <= x <= n e 0 <= y <= m, um par para o qual o  alor a expressão x * y - x² + y*/
+
+int main()
+{
+
+    //cualcular valor de x
+    //gerar os valores de x
+
+    passo_2();
+    //calcular valor e Y
+
+}
