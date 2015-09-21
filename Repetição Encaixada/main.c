40bc91b7bd17560d0cdc020e85e85d682a4e5748
daviwesley <davi.wesley@icloud.com>
2015-09-21 14:02:28 -0300

    adicionada resoluçao

--- Repetição Encaixada/main.c
+++ Repetição Encaixada/main.c
@@ -6,6 +6,30 @@
 int main()
 {
 
-   
+   int n = 2;
+    int i= 0;
+    int m = 3;int j = 0;int exp;
+    int x_max,y_max,max;
+    //cualcular valor de x
+    //gerar os valores de x
+    while(i<=n)
+    {
+        j=0;
+    while(j<=m)
+    {
+        exp = (i*j)- (i*i) + j;
+        //printf("Par Gerado: (%d, %d)', i, j);")
+        if(exp > max)
+        {
+            max = exp;
+            x_max=i;
+            y_max=j;
+        }
+        j++;
+    }
+    i++;
+    }
+   printf("Par Gerado: (%d, %d) o maximo eh %d);",x_max,y_max,max);
+
 
 }
