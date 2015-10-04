587909e206c5b460d4ff5628069d1ba22c5e81dc
daviwesley <davi.wesley@icloud.com>
2015-10-03 22:35:28 -0300

    Soma de números pares
    Dados n e n seqüências de números inteiros não-nulos, cada qual seguida
    por um 0, calcular a soma dos números pares de cada seqüência.

---
+++ while e for/somadepares.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// 1.  Dados n e n seqüências de números inteiros não-nulos, 
+// cada qual seguida por um 0, calcular a soma dos números pares de cada seqüência. 
+
+
+int main(){
+	int n, soma = 0, i;
+
+	printf("Digite um número n:");
+	scanf("%d", &n);
+
+	for(i = 0; i <= n; i++)
+	{	
+		if(i % 2 == 0)
+		{
+		soma = soma + i;	
+		}
+	}
+
+	printf("A soma dos pares é: %d \n", soma);
+
+return 0;
+}
