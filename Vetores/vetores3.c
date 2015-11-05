365c657d103475f0ebf74b55ff3de74345608965
daviwesley <davi.wesley@icloud.com>
2015-11-05 10:41:12 -0300

    adicionado pergunta do exercicio

--- Vetores/vetores3.c
+++ Vetores/vetores3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+/*Exemplo 1: Faça um programa que peça 3 números inteiros ao usuário, 
+armazene em um vetor, depois mostre o valor de cada elemento do vetor, assim como seu índice.*/
 
 int main()
 {
-    int number[3],
-        indice;
+    int number[3],indice;
 
         for(indice=0 ; indice <= 2 ; indice++)
         {
