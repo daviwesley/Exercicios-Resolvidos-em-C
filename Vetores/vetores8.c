0d7eaace1c0bcede160b4e91a6ad18156869dd78
daviwesley <davi.wesley@icloud.com>
2015-11-05 22:04:38 -0300

    Imprimir vetores na ordem reversa.

--- Vetores/vetores8.c
+++ Vetores/vetores8.c
@@ -2,8 +2,10 @@
 
 int main()
 {
-    int n[5]={23,24,65,97,12};
+    //inica todos os indicies
+	int n[5]={23,24,65,97,12};
     int i;
+	//começa na ordem descrescente de 5 a 0
     for(i=5;i>=0;i--){
         printf("%d\n",n[i]);
     }
