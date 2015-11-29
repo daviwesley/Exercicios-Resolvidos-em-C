01454fb1fe14899289b48daf967ab4aa1a7ddbc9
Davi Wesley <davi.wesley@icloud.com>
2015-11-29 17:42:57 -0300

    Criar um novo tipo usado o typedef
    A palavra reservada typedef nada mais é do que um atalho em C para que possamos nos referir a um determinado tipo existente com nomes sinônimos.

---
+++ Struct/typedef_definicao.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    typedef int meuInteiro;
+    typedef char String[20];
+ 
+    meuInteiro numero = 1;
+    String nome;
+    scanf("%[^\n]s", nome);
+ 
+    printf("A variavel do tipo 'meuInteiro' eh um int e vale %d\n", numero);
+    printf("Ja a variavel 'nome' eh uma String e armazena \"%s\"\n", nome);
+ 
+    return 0;
+}
