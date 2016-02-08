d4f46603b3d5d062b541e99f744db59973531ebc
Davi Wesley <davi.wesley@icloud.com>
2016-02-08 13:27:03 -0300

    vetores(indexes) de comandos

---
+++ Linha de Comando/linhadecomando2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int i;
+  //imprime nome do programa
+  printf("Nome do programa: \n%s",argv[0]);//argv[0] prover o nome do arquivo
+
+  return 0;
+}
