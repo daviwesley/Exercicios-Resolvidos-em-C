79a7c4c71f0b0a045941980fe2f7b4d77caaf191
Davi Wesley <davi.wesley@icloud.com>
2019-02-27 11:19:09 -0300

    draw a triangle on screen

--- opengl/aula1.c
+++ opengl/aula1.c
@@ -1,15 +1,55 @@
 #include <GL/glut.h>
-// gcc main.c -lGL -lglut -o fortinite
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define X 0
+#define Y 1
+
+float vertices[3][2] = {{-25, -25},
+							{25, -25 },
+							{0.0, 0.0}};
 void initGL(){
-	glClearColor(1.0, 0.5, 0.0, 1.0);
+	glClearColor(0.0, 0.5, 0.0, 1.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-40, 40, -30, 30, 10, 10);
+	glOrtho(-40, 40, -30, 30, 10, -10);
 	glMatrixMode(GL_MODELVIEW);
 }
 
+float ponto[2] = {0.0, 0.0};
+
+void calcularMedia(){
+	int i = 0;
+	for(i; i < 500000; i++){
+		int a = rand() % 3;
+		//printf("%i", a);
+		ponto[X] = (ponto[X] + vertices[a][X]) / 2.0;
+		ponto[Y] = (ponto[Y] + vertices[a][Y]) / 2.0;
+		glVertex2f(ponto[X], ponto[Y]);
+	}
+	
+}
+void triangulo(){
+	glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_POINTS);
+	glVertex2fv(vertices[0]);
+	glVertex2fv(vertices[1]);
+	glVertex2fv(vertices[2]);
+	calcularMedia();
+
+	// Escolher aleatoriamente um dos vertices (vertices[0] ou vertices[1] ou vertices[2])
+	// calcular o ponto médio entre ponto e o vértice escolhido
+	// ponto recebe ponto médio
+	// pinta ponto
+
+	glEnd();
+}
+
+
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
+	triangulo();
 	glFlush();
 }
 int main(int argc, char **argv)
